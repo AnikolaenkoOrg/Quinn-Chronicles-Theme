9c20fe90dad9f0ffedce8d0c833752222d276c30
Andrew Nikolaenko <andrew@contextly.com>
2023-07-13 01:35:00 +0300

    Upload theme files to repository

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Ease
+# Edition
 
-A versatile [Ghost](https://github.com/TryGhost/Ghost) theme suitable for documentation. Publish your posts or business information with ease.
+The newsletter theme for [Ghost](https://github.com/TryGhost/Ghost). A beautiful way to share stories with your growing audience.
 
-**Demo: https://ease.ghost.io**
+**Demo: https://edition.ghost.io**
 
 # Instructions
 
-1. [Download this theme](https://github.com/TryGhost/Ease/archive/main.zip)
+1. [Download this theme](https://github.com/TryGhost/Edition/archive/main.zip)
 2. Log into Ghost, and go to the `Design` settings area to upload the zip file
 
 # Development
 
-Styles are compiled using Gulp/PostCSS to polyfill future CSS spec. You'll need [Node](https://nodejs.org/), [Yarn](https://yarnpkg.com/) and [Gulp](https://gulpjs.com) installed globally. After that, from the theme's root directory:
+Edition styles are compiled using Gulp/PostCSS to polyfill future CSS spec. You'll need [Node](https://nodejs.org/), [Yarn](https://yarnpkg.com/) and [Gulp](https://gulpjs.com) installed globally. After that, from the theme's root directory:
 
 ```bash
 # Install
@@ -23,7 +23,7 @@ yarn dev
 
 Now you can edit `/assets/css/` files, which will be compiled to `/assets/built/` automatically.
 
-The `zip` Gulp task packages the theme files into `dist/ease.zip`, which you can then upload to your site.
+The `zip` Gulp task packages the theme files into `dist/edition.zip`, which you can then upload to your site.
 
 ```bash
 yarn zip
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,76 @@
-(function () {
-    var image = $('.jarallax-img');
-    if (!image) return;
+var html = document.documentElement;
+var body = document.body;
+var timeout;
+var st = 0;
 
-    var options = {
-        disableParallax: /iPad|iPhone|iPod|Android/,
-        disableVideo: /iPad|iPhone|iPod|Android/,
-        speed: 0.1,
-    };
+cover();
+featured();
+pagination(false);
 
-    image.imagesLoaded(function () {
-        image.parent().jarallax(options).addClass('initialized');
-    });
-})();
-
-(function () {
+window.addEventListener('scroll', function () {
     'use strict';
-    $('.featured-posts').owlCarousel({
-        dots: false,
-        margin: 30,
-        nav: true,
-        navText: [
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="18" height="18" fill="currentColor"><path d="M20.547 22.107L14.44 16l6.107-6.12L18.667 8l-8 8 8 8 1.88-1.893z"></path></svg>',
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="18" height="18" fill="currentColor"><path d="M11.453 22.107L17.56 16l-6.107-6.12L13.333 8l8 8-8 8-1.88-1.893z"></path></svg>',
+    if (body.classList.contains('home-template') && body.classList.contains('with-full-cover') && !document.querySelector('.cover').classList.contains('half')) {
+        if (timeout) {
+            window.cancelAnimationFrame(timeout);
+        }
+        timeout = window.requestAnimationFrame(portalButton);
+    }
+});
+
+if (document.querySelector('.cover') && document.querySelector('.cover').classList.contains('half')) {
+    body.classList.add('portal-visible');
+}
+
+function portalButton() {
+    'use strict';
+    st = window.scrollY;
+
+    if (st > 300) {
+        body.classList.add('portal-visible');
+    } else {
+        body.classList.remove('portal-visible');
+    }
+}
+
+function cover() {
+    'use strict';
+    var cover = document.querySelector('.cover');
+    if (!cover) return;
+
+    imagesLoaded(cover, function () {
+        cover.classList.remove('image-loading');
+    });
+
+    document.querySelector('.cover-arrow').addEventListener('click', function () {
+        var element = cover.nextElementSibling;
+        element.scrollIntoView({behavior: 'smooth', block: 'start'});
+    });
+}
+
+function featured() {
+    'use strict';
+    var feed = document.querySelector('.featured-feed');
+    if (!feed) return;
+
+    tns({
+        container: feed,
+        controlsText: [
+            '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M20.547 22.107L14.44 16l6.107-6.12L18.667 8l-8 8 8 8 1.88-1.893z"></path></svg>',
+            '<svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M11.453 22.107L17.56 16l-6.107-6.12L13.333 8l8 8-8 8-1.88-1.893z"></path></svg>',
         ],
+        gutter: 30,
+        loop: false,
+        nav: false,
         responsive: {
             0: {
                 items: 1,
-                slideBy: 1,
             },
             768: {
-                items: 3,
-                slideBy: 3,
+                items: 2,
             },
             992: {
-                items: 4,
-                slideBy: 4,
+                items: 3,
             },
         },
     });
-})();
+}
